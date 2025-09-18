1b50e55c493cd4dee14a5bc2b25eed5c0021d433
mvntsh <128336751+mvntsh@users.noreply.github.com>
2025-09-18 14:00:06 +0800

    Latest

--- CIS/application/views/request_v.php
+++ CIS/application/views/request_v.php
@@ -1,4 +1,4 @@
-    <div id="request-modal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div id="request-modal" tabindex="-1" data-modal-backdrop="static" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative w-xl max-w-7xl max-h-full">
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
