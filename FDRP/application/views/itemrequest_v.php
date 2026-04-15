b8f87af3dd1a1159750550546c3357df51109456
mvntsh <128336751+mvntsh@users.noreply.github.com>
2026-04-15 16:04:41 +0800

    updated version

--- FDRP/application/views/itemrequest_v.php
+++ FDRP/application/views/itemrequest_v.php
@@ -1,3 +1,44 @@
 <div class="row">
-    <div></div>
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6"></div>
+                            <div class="col-md-6">
+                                <div style="width: 200px; height: 200px; float: right;">
+                                    <canvas id="myChart"></canvas>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['Processed', 'Unprocessed'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
