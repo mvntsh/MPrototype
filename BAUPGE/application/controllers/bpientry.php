c0b111bda213e2f86f36bd1a84837adb75a94bb0
mvntsh <128336751+mvntsh@users.noreply.github.com>
2026-05-07 11:20:14 +0800

    updated

--- BAUPGE/application/controllers/bpientry.php
+++ BAUPGE/application/controllers/bpientry.php
@@ -13,6 +13,35 @@
             $this->load->view('common/navbar');
             $this->load->view('bpientry_v');
             $this->load->view('common/footer');
-        }   
+        }
+        
+        function insert_bankvalidation_c(){
+
+            $this->form_validation->set_rules("nmAccountname", "Account Name", "required");
+            $this->form_validation->set_rules("nmAccountno", "Account No", "required");
+
+
+            if ($this->form_validation->run() == TRUE) {
+                $data["success"] = true;
+            }else{
+                $data["success"] = false;
+            }
+            echo json_encode($data); 
+        }
+
+        function insert_inhousevalidation_c(){
+            $this->form_validation->set_rules("nmRfpno", "Request number", "required");
+            $this->form_validation->set_rules("nmAmount", "Inhouse Contact", "required");
+            $this->form_validation->set_rules("nmVoucherNo", "Inhouse Email", "required");
+            $this->form_validation->set_rules("nmDate", "Inhouse Email", "required");
+            $this->form_validation->set_rules("nmBatchno", "Inhouse Email", "required");
+
+            if ($this->form_validation->run() == TRUE) {
+                $data["success"] = true;
+            }else{
+                $data["success"] = false;
+            }
+            echo json_encode($data); 
+        }
     }
 ?>
--- BAUPGE/application/views/bpientry_v.php
+++ BAUPGE/application/views/bpientry_v.php
@@ -1,49 +1,83 @@
     <div class="row" style="margin: .5em;">
         <div class="col-md-6">
-            <div class="card" style="border-radius: 0px; height: 580px; overflow-y: scroll; scrollbar-width: thin; scrollbar-color: #451c06 #8c8c8c;">
+            <div class="card" style="border-radius: 0px; height: 580px; box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;">
                 <div class="card-body">
-                    <h4>Bank Transaction Details</h4>
-                    <div style="margin-top: 3em;">
-                        <div class="form-floating mb-3">
-                            <select class="form-select" id="idAccountType" aria-label="Floating label select example" style="background-color: #fce9c7; border-color: transparent;">
-                                <option value="1">Enrolled</option>
-                                <option value="2">Unenrolled</option>
-                                <option value="3">Own</option>
-                            </select>
-                            <label for="idAccountType" style="letter-spacing: -2px;">Account type</label>
+                    <form id="frmBank">
+                        <h4>Bank Details</h4>
+                        <div style="margin-top: 3em;">
+                            <div class="form-floating mb-3">
+                                <select class="form-select" id="idAccountType" aria-label="Floating label select example" style="background-color: #fce9c7; border-color: transparent;">
+                                    <option value="1">Enrolled</option>
+                                    <option value="2">Unenrolled</option>
+                                    <option value="3">Own</option>
+                                </select>
+                                <label for="idAccountType" style="letter-spacing: -2px;">Account type</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <select class="form-select" id="idTransactionType" aria-label="Floating label select example" style="background-color: #fce9c7; border-color: transparent;">
+                                    <option value="1">Accountabilities</option>
+                                    <option value="2">Advertisement</option>
+                                    <option value="3">Calamity fund</option>
+                                    <option value="4">Car-Loan</option>
+                                    <option value="5">Cash Advance</option>
+                                    <option value="6">Collection</option>
+                                    <option value="7">Constructions</option>
+                                    <option value="8">Consultancy fee</option>
+                                    <option value="9">GCash</option>
+                                    <option value="10">MCash Accountability</option>
+                                    <option value="11">ML Fund Accountability</option>
+                                    <option value="12">ML Fund Collection</option>
+                                    <option value="13">ML Influencer</option>
+                                    <option value="14">ML-Loans</option>
+                                    <option value="15">MLX Agents</option>
+                                    <option value="16">MLX Collection</option>
+                                    <option value="17">Placement Fee</option>
+                                    <option value="18">Professional Fee</option>
+                                    <option value="19">Quit Claim</option>
+                                    <option value="20">Refund</option>
+                                    <option value="21">Remittance</option>
+                                    <option value="22">Rental</option>
+                                    <option value="23">Replenishment</option>
+                                    <option value="24">Representation Expense</option>
+                                    <option value="25">Security Guard</option>
+                                    <option value="26">Software Maintenance</option>
+                                    <option value="27">Sponsorship</option>
+                                    <option value="28">Supplier</option>
+                                    <option value="29">SW-Rental</option>
+                                    <option value="30">Taxes & Licenses</option>
+                                    <option value="31">Telecomms</option>
+                                    <option value="32">Vis-Advertising</option>
+                                    <option value="33">Vis-Supplier</option>
+                                </select>
+                                <label for="idTransactionType" style="letter-spacing: -2px;">Transaction type</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" name="nmAccountname" class="form-control" id="idAccountname" placeholder="1" style="background-color: #fce9c7; border-color: transparent;" autocomplete="off">
+                                <label for="idAccountname" style="letter-spacing: -2px;"> Account Name.</label>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" name="nmAccountno" class="form-control" id="idAccountno" placeholder="1" style="background-color: #fce9c7; border-color: transparent;" autocomplete="off">
+                                <label for="idAccountno" style="letter-spacing: -2px;"> Account No.</label>
+                            </div>
                         </div>
-                        <div class="form-floating mb-3">
-                            <select class="form-select" id="idTransactionType" aria-label="Floating label select example" style="background-color: #fce9c7; border-color: transparent;">
-                                <option value="1">Enrolled</option>
-                                <option value="2">Unenrolled</option>
-                                <option value="3">Own</option>
-                            </select>
-                            <label for="idTransactionType" style="letter-spacing: -2px;">Transaction type</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="text" name="nmAccountname" class="form-control" id="idAccountname" placeholder="1" style="background-color: #fce9c7; border-color: transparent;">
-                            <label for="idAccountname" style="letter-spacing: -2px;"> Account Name.</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="text" name="nmAccountno" class="form-control" id="idAccountno" placeholder="1" style="background-color: #fce9c7; border-color: transparent;">
-                            <label for="idAccountno" style="letter-spacing: -2px;"> Account No.</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="text" name="nmBatchno" class="form-control" id="idBatch" placeholder="1" style="background-color: #fce9c7; border-color: transparent;">
-                            <label for="idBatch" style="letter-spacing: -2px;"> Batch No.</label>
+                    </form>
+                    <div class="row">
+                        <div class="col-md-6"></div>
+                        <div class="col-md-6 d-grid">
+                            <button id="btnProceed" class="btn btn-primary btn-lg" style="letter-spacing: -2px; border-radius: 0px;"><i class="fa-solid fa-arrow-right" style="color: rgb(239, 242, 238);"></i>PROCEED.</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
         <div class="col-md-6">
-            <div class="card" style="border-radius: 0px; height: 580px; overflow-y: scroll; scrollbar-width: thin; scrollbar-color: #451c06 #8c8c8c;">
+            <div class="card" style="border-radius: 0px; height: 580px; box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;" id="idInhouseDetails">
                 <div class="card-body">
-                    <h4>In-house Transaction Details</h4>
+                    <h4>In-house Details</h4>
                     <div style="margin-top: 3em;">
                         <div class="form-floating mb-3">
-                            <input type="text" name="nmRfpno" class="form-control" id="idRFPNo" placeholder="1" style="background-color: #fce9c7; border-color: transparent;">
-                            <label for="idRFPNo" style="letter-spacing: -2px;"> RFP No.</label>
+                            <input type="text" name="nmRequestno" class="form-control" id="idRequestNo" placeholder="1" style="background-color: #fce9c7; border-color: transparent;">
+                            <label for="idRequestNo" style="letter-spacing: -2px;"> Request No.</label>
                         </div>
                         <div class="form-floating mb-3">
                             <input type="text" name="nmAmount" class="form-control" id="idAmount" placeholder="1" style="background-color: #fce9c7; border-color: transparent;">
@@ -56,6 +90,10 @@
                         <div class="form-floating mb-3">
                             <input type="date" name="nmDate" class="form-control" id="idDate" placeholder="1" style="background-color: #fce9c7; border-color: transparent;">
                             <label for="idDate" style="letter-spacing: -2px;"> Date</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" name="nmBatchno" class="form-control" id="idBatch" placeholder="1" style="background-color: #fce9c7; border-color: transparent;">
+                            <label for="idBatch" style="letter-spacing: -2px;"> Batch No.</label>
                         </div>
                     </div>
                     <div class="row">
@@ -70,6 +108,28 @@
     </div>
     <script type="text/javascript">
         $(document).ready(function() {
+            $("#idInhouseDetails").css("display", "none");
+
+            $("#btnProceed").click(function(){
+                $.ajax({
+                    type:'POST',
+                    url:'<?php echo base_url("bpientry/insert_bankvalidation_c"); ?>',
+                    data: $("#frmBank").serialize(),
+                    dataType:'json',
+                    success:function(response){
+                        if(response.success){
+                            $("#idInhouseDetails").fadeIn(500);
+                            $("#idAccountname,#idAccountno").removeClass("is-invalid");
+                            $("#idAccountname,#idAccountno").addClass("is-valid");
+                            $("#idRequestNo").focus();
+                        }else{
+                            $("#idAccountname,#idAccountno").addClass("is-invalid");
+                            $("#idInhouseDetails").fadeOut(500);
+                        }
+                    }
+                })
+            })
+
             $('#idAccountno,#idBatch,#idRFPNo,#idVoucherNo').keypress(function(e){    
         
                 var charCode = (e.which) ? e.which : event.keyCode    
