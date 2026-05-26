729ae1e8c3909d06d1403ac7d381c53d0717854a
mvntsh <128336751+mvntsh@users.noreply.github.com>
2026-05-26 16:55:50 +0800

    updated

---
+++ BAUPGE/application/models/read_m.php
@@ -0,0 +1,12 @@
+<?php
+    class read_m extends CI_Model{
+        function __construct() {
+            parent::__construct();
+            $this->load->database();
+        }
+
+        function showData_m(){
+            
+        }
+    }
+?>
--- BAUPGE/application/views/archives_v.php
+++ BAUPGE/application/views/archives_v.php
@@ -4,6 +4,95 @@
             <div class="card" style="border-radius: 0px; border-color: transparent; background-color: transparent;">
                 <div class="card-body">
                     <h1 class="text-center" style="letter-spacing: -1px; text-transform: uppercase;"><i class="fa-solid fa-box-archive" style="color: rgb(156, 12, 12);"></i> Archives</h1>
+                    <div class="row">
+                        <div class="col-md-8"></div>
+                        <div class="col-md-4">
+                            <div class="form-floating mb-3">
+                                <input type="search" class="form-control" id="idnmSearch" placeholder="name@example.com">
+                                <label for="idnmSearch">Search</label>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="card" style="overflow-y: scroll; height: 420px; scrollbar-width: thin; scrollbar-color: #828282 #e6e8e6; border-radius: 0px; border-color: transparent; background-color: transparent; box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;">
+                        <table class="table table-secondary table-hover table-striped" style="border-color: transparent; border-radius: 0px;">
+                            <thead>
+                                <tr style="position: sticky; top: 0; height: 3.5em; vertical-align: middle; text-align: center; background-color: #37692b; color: #cce8c5;">
+                                    <td style="width: 10%; text-transform: uppercase;">Request #</td>
+                                    <td style="width: 20%; text-transform: uppercase;">Name</td>
+                                    <td style="width: 10%; text-transform: uppercase;">Account #</td>
+                                    <td style="width: 15%; text-transform: uppercase;">Voucher Date</td>
+                                    <td style="width: 15%; text-transform: uppercase;">Amount</td>
+                                    <td style="width: 20%; text-transform: uppercase;">Description</td>
+                                    <td style="width: 10%; text-transform: uppercase;">Status</td>
+                                </tr>
+                            </thead>
+                            <tbody style="zoom: 85%;">
+                                <tr>
+                                    <td style="text-align: center; vertical-align: middle;">01000001</td>
+                                    <td style="ertical-align: middle;">Marvin B. Batitay Marvin B. Batitay</td>
+                                    <td style="text-align: center; vertical-align: middle;">1902319019023190</td>
+                                    <td style="text-align: center; vertical-align: middle;">2026-05-26</td>
+                                    <td style="text-align: right; vertical-align: middle;">1,000.00</td>
+                                    <td style="text-align: center; vertical-align: middle;">sampleahahsdflkjhadskljfladsjfaldskjfalkdfj;alkj;lakj;lakjf;lajkf;lkajfjksdalfkjasdlkf</td>
+                                    <td style="text-align: center; vertical-align: middle;"><span class="badge bg-secondary">Unprocess</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center; vertical-align: middle;">01000001</td>
+                                    <td style="vertical-align: middle;">Marvin B. Batitay Marvin B. Batitay</td>
+                                    <td style="text-align: center; vertical-align: middle;">1902319019023190</td>
+                                    <td style="text-align: center; vertical-align: middle;">2026-05-26</td>
+                                    <td style="text-align: right; vertical-align: middle;">1,000.00</td>
+                                    <td style="text-align: center; vertical-align: middle;">sampleahahsdflkjhadskljfladsjfaldskjfalkdfj;alkj;lakj;lakjf;lajkf;lkajfjksdalfkjasdlkf</td>
+                                    <td style="text-align: center; vertical-align: middle;"><span class="badge bg-secondary">Unprocess</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center; vertical-align: middle;">01000001</td>
+                                    <td style="vertical-align: middle;">Marvin B. Batitay Marvin B. Batitay</td>
+                                    <td style="text-align: center; vertical-align: middle;">1902319019023190</td>
+                                    <td style="text-align: center; vertical-align: middle;">2026-05-26</td>
+                                    <td style="text-align: right; vertical-align: middle;">1,000.00</td>
+                                    <td style="text-align: center; vertical-align: middle;">sampleahahsdflkjhadskljfladsjfaldskjfalkdfj;alkj;lakj;lakjf;lajkf;lkajfjksdalfkjasdlkf</td>
+                                    <td style="text-align: center; vertical-align: middle;"><span class="badge bg-secondary">Unprocess</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center; vertical-align: middle;">01000001</td>
+                                    <td style="vertical-align: middle;">Marvin B. Batitay Marvin B. Batitay</td>
+                                    <td style="text-align: center; vertical-align: middle;">1902319019023190</td>
+                                    <td style="text-align: center; vertical-align: middle;">2026-05-26</td>
+                                    <td style="text-align: right; vertical-align: middle;">1,000.00</td>
+                                    <td style="text-align: center; vertical-align: middle;">sampleahahsdflkjhadskljfladsjfaldskjfalkdfj;alkj;lakj;lakjf;lajkf;lkajfjksdalfkjasdlkf</td>
+                                    <td style="text-align: center; vertical-align: middle;"><span class="badge bg-secondary">Unprocess</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center; vertical-align: middle;">01000001</td>
+                                    <td style="vertical-align: middle;">Marvin B. Batitay Marvin B. Batitay</td>
+                                    <td style="text-align: center; vertical-align: middle;">1902319019023190</td>
+                                    <td style="text-align: center; vertical-align: middle;">2026-05-26</td>
+                                    <td style="text-align: right; vertical-align: middle;">1,000.00</td>
+                                    <td style="text-align: center; vertical-align: middle;">sampleahahsdflkjhadskljfladsjfaldskjfalkdfj;alkj;lakj;lakjf;lajkf;lkajfjksdalfkjasdlkf</td>
+                                    <td style="text-align: center; vertical-align: middle;"><span class="badge bg-secondary">Unprocess</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center; vertical-align: middle;">01000001</td>
+                                    <td style="vertical-align: middle;">Marvin B. Batitay Marvin B. Batitay</td>
+                                    <td style="text-align: center; vertical-align: middle;">1902319019023190</td>
+                                    <td style="text-align: center; vertical-align: middle;">2026-05-26</td>
+                                    <td style="text-align: right; vertical-align: middle;">1,000.00</td>
+                                    <td style="text-align: center; vertical-align: middle;">sampleahahsdflkjhadskljfladsjfaldskjfalkdfj;alkj;lakj;lakjf;lajkf;lkajfjksdalfkjasdlkf</td>
+                                    <td style="text-align: center; vertical-align: middle;"><span class="badge bg-secondary">Unprocess</span></td>
+                                </tr>
+                                <tr>
+                                    <td style="text-align: center; vertical-align: middle;">01000001</td>
+                                    <td style="vertical-align: middle;">Marvin B. Batitay Marvin B. Batitay</td>
+                                    <td style="text-align: center; vertical-align: middle;">1902319019023190</td>
+                                    <td style="text-align: center; vertical-align: middle;">2026-05-26</td>
+                                    <td style="text-align: right; vertical-align: middle;">1,000.00</td>
+                                    <td style="text-align: center; vertical-align: middle;">sampleahahsdflkjhadskljfladsjfaldskjfalkdfj;alkj;lakj;lakjf;lajkf;lkajfjksdalfkjasdlkf</td>
+                                    <td style="text-align: center; vertical-align: middle;"><span class="badge bg-secondary">Unprocess</span></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div> 
                 </div>
             </div>
         </div>
